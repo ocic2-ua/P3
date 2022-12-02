5b7bff546a6d5b780d333a89b108e9b35eb0a13b
Óscar <98819300+ocic2-ua@users.noreply.github.com>
2022-12-02 15:38:41 +0100

    Delete prac directory

--- prac/P2/src/es/ua/dlsi/prog3/p2/exceptions/NoTyreTypeException.java
+++
@@ -1,5 +0,0 @@
-package es.ua.dlsi.prog3.p2.exceptions;
-
-public class NoTyreTypeException extends Throwable{
-
-}
--- prac/P3/src/es/ua/dlsi/prog3/p3/highlevel/exceptions/NoLineForPrintingException.java
+++
@@ -1,5 +0,0 @@
-package es.ua.dlsi.prog3.p3.highlevel.exceptions;
-
-public class NoLineForPrintingException extends Throwable{
-
-}
